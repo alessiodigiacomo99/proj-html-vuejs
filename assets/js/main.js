36bf180aa260f4fcecd19d0b0db35e0a75f6f56e
alessio <alessiodg99.adg@gmail.com>
2021-01-19 18:13:32 +0100

    fine sezione courses

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,74 @@
 let root = new Vue ({
     el: '#root',
     data:{
-        partners:[],
-        courses:[],
+        partners:[
+            {
+                img: '01'
+            },
+            {
+                img: '02'
+            },
+            {
+                img: '03'
+            },
+            {
+                img: '04'
+            },
+            {
+                img: '05'
+            },
+            {
+                img: '06'
+            },
+            {
+                img: '07'
+            },
+        ],
+        courses:[
+            {
+                img: 'course-02-480x298.jpg',
+                costo: '$40.00',
+                info: 'Learning to Write as a Professional Author',
+                lezioni: 20,
+                studenti: 50,
+            },
+            {
+                img: 'stock-full-hd-03-480x298.jpg',
+                costo: 'Free',
+                info: 'Customer-centric Info-Tech Strategies',
+                lezioni: 24,
+                studenti: 769,
+            },
+            {
+                img: 'stock-full-hd-04-480x298.jpg',
+                costo: '$19.00',
+                info: 'Open Programming Courses for Everyone: Python',
+                lezioni: 17,
+                studenti: 62,
+            },
+            {
+                img: 'stock-full-hd-06-480x298.jpg',
+                costo: '$26.00',
+                info: 'Accademic Listening and Note-taking',
+                lezioni: 14,
+                studenti: 67,
+            },
+            {
+                img: 'course-featured-image-01-480x298.jpg',
+                costo: '$39.00',
+                info: 'Master jQuery in a Short Period of Time',
+                lezioni: 6,
+                studenti: 51,
+            },
+            {
+                img: 'stock-full-hd-05-480x298.jpg',
+                costo: '$59.00',
+                info: 'Introduction to Javascript for Beginners',
+                lezioni: 14,
+                studenti: 76,
+            },
+            
+        ],
         recensioni:[],
     },
     methods:{
